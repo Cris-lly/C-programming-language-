c07b65c8be64135be54933d4745019fc7d0c151c
Cris-lly <86075123+Cris-lly@users.noreply.github.com>
2022-02-08 16:23:00 -0300

    Add files via upload
    Gravar um texto em um outro arquivo com outra formatação

---
+++ Exercicio_sobre_arquivos.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <locale.h>
+
+/*
+Problema: ler um arquivo contendo um texto em minúsculo
+e criar outro arquivo com o texto em maiúsculo
+*/
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    FILE *texto1, *texto2;
+    texto1 = fopen("..\\linguagem C\\minusculo.txt", "r");
+    texto2 = fopen("..\\linguagem C\\maiusculo.txt", "w");
+    if(texto1 == NULL || texto2 == NULL){
+        printf("ERRO!");
+        exit(1);
+    }
+    char aux = getc(texto1);
+    while(aux != EOF){
+        fputc(toupper(aux), texto2);
+        aux = getc(texto1);
+    }
+    fclose(texto1);
+    fclose(texto2);
+    return 0;
+
+}
